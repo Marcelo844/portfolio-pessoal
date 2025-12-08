e40c9f5311762e93477bb1c9a5f237262fafb52d
Marcelo Rangel Barros <marcelorangelb2006@gmail.com>
2025-12-07 21:59:23 -0300

    Adicionando mais projetos

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -13,14 +13,19 @@ const projects = [
     link: 'https://github.com/Marcelo844/catalogo-filmes-react'
   },
   {
-    title: 'Batalha Naval Em Java',
-    description: 'Recriação do jogo Batalha Naval utilizando Java como linguagem de programação. Jogado diretamente pelo terminal.',
-    link: 'https://github.com/brunowasch/java-navalbattle'
-  },
-  {
     title: 'Dashboard interativo utilizando Python com StreamLit',
     description: 'Dashboard interativo em Streamlit que analisa dados do Kaggle sobre características de veículos e seus respectivos preços (MSRP).',
     link: 'https://github.com/Marcelo844/Trabalho-Dashboard'
+  },
+  {
+    title: 'Sistema de Caixa utilizando Typescript + React + Vite + Supabase',
+    description: 'Um sistema de caixa moderno, desenvolvido em Node.js + Express + React + Vite + Supabase, com autenticação de usuários e funcionalidades completas de gerenciamento de fluxo de caixa.',
+    link: 'https://github.com/Marcelo844/sistema-de-caixa'
+  },
+  {
+    title: 'Mini Blog com Bootstrap e AngularJS',
+    description: 'Este projeto é um mini blog estático desenvolvido com HTML, CSS, Bootstrap e AngularJS, com a proposta futura de consumir uma API REST para gerenciamento de postagens.',
+    link: 'https://github.com/Marcelo844/Mini-blog-utilizando-Bootstrap-com-API-de-AngularJS'
   }
 ];
 
